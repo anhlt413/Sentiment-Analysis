d5894be865e2323976a626eb22df28e7b437f0f4
Le Tuan Anh <85450301+anhlt413@users.noreply.github.com>
2021-10-30 17:34:52 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+Bài làm gồm các phần:
+1. BàiLàm.ipynb: file jupyter notebook chứa bài làm của cả 3 bài
+2. model.py: chứa class để xây dựng model cho bài
+3. vocab.py: chứa class để xây dựng vocab cho bài
+4. dataset.txt: tập dữ liệu gốc cho bài 3
+5. model.bin: file lưu model
+6. train.txt, dev.txt, test.txt: lần lượt là 3 tập dữ liệu được chia ra từ dataset.txt ở bài 3.1
+7. toy_data.txt: file để test hàm ở bài 1
+8. utils.py: chứa các hàm thông dụng được sử dụng trong bài
+   
+Thắc mắc xin gửi về mail:
+anh.lt172947@sis.hust.edu.vn  (Lê Tuấn Anh)
+
---
+++ model.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+import torch.utils.data
+import numpy as np
+from vocab import *
+
+
+class RNNModel(nn.Module):
+    def __init__(self,
+            vocab_size,
+            embed_dim=50,
+            embedding=None,
+            use_embedding=True,
+            rnn_cell_class=nn.LSTM,
+            hidden_dim=50,
+            bidirectional=False,
+            freeze_embedding=False):
+
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.use_embedding = use_embedding
+        self.embed_dim = embed_dim
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+        self.freeze_embedding = freeze_embedding
+        # Graph
+        if self.use_embedding:
+            self.embedding = self._define_embedding(
+                embedding, vocab_size, self.embed_dim, self.freeze_embedding)
+            self.embed_dim = self.embedding.embedding_dim
+        self.rnn = rnn_cell_class(
+            input_size=self.embed_dim,
+            hidden_size=hidden_dim,
+            batch_first=True,
+            bidirectional=bidirectional)
+
+    def forward(self, X, seq_lengths):
+        if self.use_embedding:
+            X = self.embedding(X)
+        embs = torch.nn.utils.rnn.pack_padded_sequence(
+            X,
+            batch_first=True,
+            lengths=seq_lengths.cpu(),
+            enforce_sorted=False)
+        outputs, state = self.rnn(embs)
+        return outputs, state
+
+    @staticmethod
+    def _define_embedding(embedding, vocab_size, embed_dim, freeze_embedding):
+        if embedding is None:
+            emb = nn.Embedding(vocab_size, embed_dim)
+            emb.weight.requires_grad = not freeze_embedding
+            return emb
+        elif isinstance(embedding, np.ndarray):
+            embedding = torch.FloatTensor(embedding)
+            return nn.Embedding.from_pretrained(
+                embedding, freeze=freeze_embedding)
+        else:
+            return embedding
+
+
+class RNNClassifierModel(nn.Module):
+    def __init__(self, rnn, output_dim, classifier_activation, vocab):
+        super().__init__()
+        self.rnn = rnn
+        self.vocab = vocab
+        self.output_dim = output_dim
+        self.hidden_dim = self.rnn.hidden_dim
+        self.device = "cpu"
+        if self.rnn.bidirectional:
+            self.classifier_dim = self.hidden_dim * 2
+        else:
+            self.classifier_dim = self.hidden_dim
+        self.hidden_layer = nn.Linear(
+            self.classifier_dim, self.hidden_dim)
+        self.classifier_activation = classifier_activation
+        self.classifier_layer = nn.Linear(
+            self.hidden_dim, self.output_dim)
+
+    def forward(self, X, seq_lengths):
+        X = self.vocab.to_input_tensor(X, self.device)
+        outputs, state = self.rnn(X, seq_lengths)
+        state = self.get_batch_final_states(state)
+        if self.rnn.bidirectional:
+            state = torch.cat((state[0], state[1]), dim=1)
+        h = self.classifier_activation(self.hidden_layer(state))
+        logits = self.classifier_layer(h)
+        return logits
+
+    def get_batch_final_states(self, state):
+        if self.rnn.rnn.__class__.__name__ == 'LSTM':
+            return state[0].squeeze(0)
+        else:
+            return state.squeeze(0)
+
+    def predict(self, X, seq_lengths):
+        logits = self.forward(X,seq_lengths)
+        return logits.argmax(-1)
+
+    @staticmethod
+    def load(model_path: str):
+        """ Load the model from a file.
+        @param model_path (str): path to model
+        """
+        params = torch.load(model_path, map_location=lambda storage, loc: storage)
+        args = params['args']
+        model = RNNClassifierModel(vocab=params['vocab'], rnn = params['rnn'], **args)
+        model.load_state_dict(params['state_dict'])
+
+        return model
+
+    def save(self, path: str):
+        """ Save the odel to a file.
+        @param path (str): path to the model
+        """
+        print('save model parameters to [%s]' % path)
+
+        params = {
+            'args': dict(output_dim = self.output_dim, classifier_activation = self.classifier_activation),
+            'vocab': self.vocab,
+            'rnn': self.rnn,
+            'state_dict': self.state_dict()
+        }
+
+        torch.save(params, path)
